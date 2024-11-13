d615ee3ba17a64971a37f96d641d20af09aca92e
Aliza Engle <lizzisue42@protonmail.com>
2024-11-13 11:33:17 -0500

    y

--- resources/js/script/script.js
+++ resources/js/script/script.js
@@ -2,3 +2,9 @@ document.getElementById("num1").oneclick = function write1()
 {
 document.getElementById("screen").innerHTML = "1";
 }
+
+
+document.getElementById("clear").onclick = function writeClear()
+{
+  document.getElementById("screen").innerHTML = "";  
+}
